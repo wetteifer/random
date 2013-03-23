1b2d03c5dbee5ded7e6e78c8a443536ac6278da4
wetteifer <wetteifer@gmail.com>
2013-03-23 09:47:19 -0700

    Update counter.rb
    mas mas corto

--- counter/counter.rb
+++ counter/counter.rb
@@ -2,20 +2,16 @@
 
 require 'tempfile'
 
-def count(file, number)
+def count(f, n)
   start = Time.now
-  1.upto(number) { |x| file.write x }
+  1.upto(n) { |x| f.write x }
   (Time.now - start) * 1000
 end
 
-if __FILE__ == $0
-  Tempfile.open('counter') do |file|
-    puts '+------------------+---------------+'
-    puts '| Numeros impresos | Duracion (ms) |'
-    puts '+------------------+---------------+'
-    (ARGV.length > 0 ? ARGV.collect { |i| i.to_i } : 10.step(100, 10)).each do |number|
-      puts '| %16d | %13.2f |' % [number, count(file, number)]
-    end
-    puts '+------------------+---------------+'
-  end
+Tempfile.open('counter') do |f|
+  puts '+------------------+---------------+'
+  puts '| Numeros impresos | Duracion (ms) |'
+  puts '+------------------+---------------+'
+  (ARGV.empty?? 10.step(100, 10) : ARGV.collect { |i| i.to_i }).each { |n| puts '| %16d | %13.2f |' % [n, count(f, n)] }
+  puts '+------------------+---------------+'
 end
