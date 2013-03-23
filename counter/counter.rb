c8e1632499bfc56efd5dd0fd79238a4ed46dc0ad
Darío Lumbreras <wetteifer@gmail.com>
2013-03-22 19:32:23 -0700

    indent

--- counter/counter.rb
+++ counter/counter.rb
@@ -3,20 +3,20 @@
 require 'tempfile'
 
 def count(file, number)
-	start = Time.now
-	1.upto(number) { |x| file.write x }
-	duration = (Time.now - start) * 1000
-	yield duration
+  start = Time.now
+  1.upto(number) { |x| file.write x }
+  duration = (Time.now - start) * 1000
+  yield duration
 end
 
 if __FILE__ == $0
-	Tempfile.open('counter') do |file|
-		puts '+------------------+---------------+'
-		puts '| Numeros impresos | Duracion (ms) |'
-		puts '+------------------+---------------+'
-		(ARGV.length > 0 ? ARGV.collect { |i| i.to_i } : 10.step(100, 10)).each do |number|			
-			count(file, number) { |duration| puts '| %16d | %13.2f |' % [number, duration] }			
-		end
-		puts '+------------------+---------------+'
-	end
+  Tempfile.open('counter') do |file|
+    puts '+------------------+---------------+'
+    puts '| Numeros impresos | Duracion (ms) |'
+    puts '+------------------+---------------+'
+	(ARGV.length > 0 ? ARGV.collect { |i| i.to_i } : 10.step(100, 10)).each do |number|         
+      count(file, number) { |duration| puts '| %16d | %13.2f |' % [number, duration] }            
+    end
+    puts '+------------------+---------------+'
+  end
 end
